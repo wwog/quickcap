ba870ca4a19158c2db017bf8f2779109b5899e07
wwog <871782513@qq.com>
2025-12-08 10:33:53 +0800

    -添加必要库，构建App结构体

--- src/app/app.rs
+++ src/app/app.rs
@@ -3,26 +3,42 @@ use std::collections::HashMap;
 use super::window::AppWindow;
 use tao::event::{Event, WindowEvent};
 use tao::event_loop::{ControlFlow, DeviceEventFilter, EventLoop};
+use tao::monitor::MonitorHandle;
 use tao::window::{WindowBuilder, WindowId};
 
 pub struct App {
     event_loop: EventLoop<()>,
     windows: HashMap<WindowId, AppWindow>,
+    monitors: Vec<MonitorHandle>,
 }
 
 impl App {
     pub fn new() -> Self {
         let event_loop = EventLoop::new();
+        let monitors: Vec<MonitorHandle> = event_loop.available_monitors().collect();
+        log::info!("Found {} monitors", monitors.len());
+        for monitor in &monitors {
+            log::info!(
+                "{}, {:?}, {:?}",
+                monitor.name().unwrap_or("Unknown".to_string()),
+                monitor.position(),
+                monitor.size()
+            );
+        }
         event_loop.set_device_event_filter(DeviceEventFilter::Never);
-        let window_builder = WindowBuilder::new();
-        let window = window_builder.build(&event_loop).unwrap();
-        Self { event_loop, windows: HashMap::new() }
+
+        Self {
+            event_loop,
+            windows: HashMap::new(),
+            monitors,
+        }
     }
 }
 
 impl App {
-    pub fn run(self) {
-        self.event_loop.run(move |event, target, control_flow| {
+    pub fn run(mut self) {
+        self.create_window();
+        self.event_loop.run(move |event, _target, control_flow| {
             *control_flow = ControlFlow::Wait;
 
             println!("{:?}", event);
@@ -43,5 +59,20 @@ impl App {
                 _ => (),
             }
         })
+    }
+
+    pub fn create_window(&mut self) -> () {
+        for (index, monitor) in self.monitors.iter().enumerate() {
+            let title = monitor.name().unwrap_or(format!("quickcap-{}", index));
+            log::info!("Creating window for monitor: {:?}", title);
+            let window = WindowBuilder::new()
+                .with_title(title)
+                .with_inner_size(monitor.size())
+                .build(&self.event_loop)
+                .unwrap();
+            let window_id = window.id();
+            let app_window = AppWindow::new(window);
+            self.windows.insert(window_id, app_window);
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+use env_logger;
+use log::LevelFilter;
 use quickcap::App;
 
 fn main() {
+    env_logger::Builder::from_default_env()
+        .filter_level(LevelFilter::Info)
+        .init();
+    log::info!("Starting QuickCap");
     let app = App::new();
-    app.run();
+    // app.run();
 }
