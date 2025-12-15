c5c81fe8497edb3e5f3b04a72c9245a65adefa75
wwog <871782513@qq.com>
2025-12-15 09:53:33 +0800

    - finally

--- src/app/capscreen/enumerate/macos.rs
+++ src/app/capscreen/enumerate/macos.rs
@@ -1,7 +1,42 @@
-use objc2_core_foundation::{CFArray, CFDictionary, CFRetained, CFShow};
-use objc2_core_graphics::{CGWindowListCopyWindowInfo, CGWindowListOption, kCGNullWindowID};
+use objc2_core_foundation::{
+    CFArray, CFDictionary, CFNumber, CFRetained, CFShow, CFString, CFType,
+};
+use objc2_core_graphics::{
+    CGWindowListCopyWindowInfo, CGWindowListOption, kCGNullWindowID, kCGWindowBounds, kCGWindowName,
+};
+
+use crate::app::capscreen::enumerate::structs::Rect;
 
 use super::structs::WindowInfo;
+
+unsafe fn get_number(dict: &CFDictionary<CFString, CFType>, key: &'static str) -> Option<f64> {
+    let key = CFString::from_static_str(key);
+
+    let value = dict.get(&key)?;
+    let number = value.downcast_ref::<CFNumber>()?;
+    number.as_f64()
+}
+
+unsafe fn extract_bounds(window: &CFDictionary<CFString, CFType>) -> Option<Rect> {
+    unsafe {
+        let bounds_value = window.get(&kCGWindowBounds)?;
+        let raw_dict = bounds_value.downcast_ref::<CFDictionary>()?;
+
+        let bounds_dict: &CFDictionary<CFString, CFType> = raw_dict.cast_unchecked();
+
+        let x = get_number(bounds_dict, "X")?;
+        let y = get_number(bounds_dict, "Y")?;
+        let width = get_number(bounds_dict, "Width")?;
+        let height = get_number(bounds_dict, "Height")?;
+
+        Some(Rect {
+            x,
+            y,
+            width,
+            height,
+        })
+    }
+}
 
 pub fn enumerate_windows(display_id: u32) -> Vec<WindowInfo> {
     let mut window_infos: Vec<WindowInfo> = vec![];
@@ -16,11 +51,27 @@ pub fn enumerate_windows(display_id: u32) -> Vec<WindowInfo> {
             return window_infos;
         };
 
-        let windows: CFRetained<CFArray<CFDictionary>> = CFRetained::cast_unchecked(raw_windows);
+        let windows: CFRetained<CFArray<CFDictionary<CFString, CFType>>> =
+            CFRetained::cast_unchecked(raw_windows);
 
         for window in windows.iter() {
-            let dict: &CFDictionary = window.as_opaque();
-            CFShow(Some(dict));
+            let name: String = if let Some(value) = window.get(&kCGWindowName) {
+                if let Some(cf_str) = value.downcast_ref::<CFString>() {
+                    cf_str.to_string()
+                } else {
+                    String::new()
+                }
+            } else {
+                String::new()
+            };
+            let bounds = extract_bounds(&window);
+            if let Some(bounds) = bounds {
+                window_infos.push(WindowInfo { name, bounds });
+            } else {
+                CFShow(Some(&window));
+                log::warn!("Failed to extract bounds for window: {}", name);
+                continue;
+            }
         }
 
         window_infos
@@ -33,6 +84,7 @@ mod tests {
 
     #[test]
     fn test_enumerate_windows() {
-        enumerate_windows(1);
+        let window_infos = enumerate_windows(1);
+        println!("window_infos: {:#?}", window_infos);
     }
 }
--- src/app/capscreen/enumerate/structs.rs
+++ src/app/capscreen/enumerate/structs.rs
@@ -1,4 +1,12 @@
-use wry::Rect;
+
+#[allow(dead_code)]
+#[derive(Debug, Clone)]
+pub struct Rect {
+    pub x: f64,
+    pub y: f64,
+    pub width: f64,
+    pub height: f64,
+}
 
 #[allow(dead_code)]
 #[derive(Debug, Clone)]
