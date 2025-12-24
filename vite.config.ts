13fb9162893a165966805779489509ffbcc4c1cf
wwog <871782513@qq.com>
2025-12-24 18:24:42 +0800

    -

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import path from "node:path";
-import { defineConfig, Plugin } from "vite";
+import { defineConfig, type Plugin } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile"
 
-const outDir = path.resolve(__dirname, "../src/app");
+const outDir = path.resolve(__dirname, "./src/app");
 
 function htmlMinify(): Plugin {
   return {
